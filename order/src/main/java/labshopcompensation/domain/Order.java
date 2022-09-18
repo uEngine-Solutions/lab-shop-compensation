a17d4897969fbfa035b9df55f9882dc229592031
Jinyoung Jang <jyjang@uengine.org>
2022-09-18 15:31:08 +0900

    commit

--- order/src/main/java/labshopcompensation/domain/Order.java
+++ order/src/main/java/labshopcompensation/domain/Order.java
@@ -47,6 +47,9 @@ public class Order {
 
     }
 
+    @PreRemove
+    public void onPreRemove() {}
+
     public static OrderRepository repository() {
         OrderRepository orderRepository = OrderApplication.applicationContext.getBean(
             OrderRepository.class
